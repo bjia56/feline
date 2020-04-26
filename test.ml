706050c440e550d09b5e15a22ad5dba992b3e514
Neeraj Ramachandran <ramachandraneeraj@gmail.com>
2020-04-25 23:09:47 -0400

    committing test.ml to check test programs

---
+++ test.ml
@@ -0,0 +1,7 @@
+open Sast
+
+let _ =
+  let lexbuf = Lexing.from_channel stdin in
+  let program = Parser.program Scanner.token lexbuf in
+  (* let sprogram = *) Semant.check (program.classes, program.functions, program.globals) 
+  (*in print_endline (string_of_sprogram sprogram) *)
