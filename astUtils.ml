78bb8fe56279318de6078898feaa68aae7a3b2e7
Brett Jia <dev.bjia56@gmail.com>
2020-04-29 03:03:25 +0000

    code formatting

--- astUtils.ml
+++ astUtils.ml
@@ -2,10 +2,10 @@ open Ast
 
 (* Pretty printing functions *)
 let string_of_typ = function
-    | Void -> "void"
-    | Null -> "null"
-    | Int -> "int"
-    | String -> "string"
-    | Bool -> "bool"
-    | Exception s -> "exception " ^ s
-    | TypIdent s -> "custom " ^ s
+  | Void -> "void"
+  | Null -> "null"
+  | Int -> "int"
+  | String -> "string"
+  | Bool -> "bool"
+  | Exception s -> "exception " ^ s
+  | TypIdent s -> "custom " ^ s
